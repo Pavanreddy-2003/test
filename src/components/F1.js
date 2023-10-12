7eb9c5d0c71a7c97837017dc724bd7f0ece51857
unknown <pavanreddy2809@gmail.com>
2023-10-12 22:14:00 +0530

    first commit

---
+++ src/components/F1.js
@@ -0,0 +1,31 @@
+import Axios from "axios";
+import {useEffect} from "react";
+function F1()
+{
+    //HTTP request we need to place inside the lifecycle methods
+    //Bt as we are working with function component there is no lifecycle methods
+    //Instead we have the concept called Hooks which helps us to handle the HTTP requests
+    //useEffect() is used to handle the side effects
+    
+    useEffect(()=>{
+    Axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>{
+        if(res.status === 200) //200 - OK
+        {
+            console.log(res.data);
+        }
+        else{
+            Promise.reject();
+        }
+    })
+    .catch((err)=>alert(err));
+},[])
+
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default F1;
